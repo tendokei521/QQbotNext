18407842e4d309af431a99d78378515e8f06495f
tendokei521 <tendokei521@gmail.com>
2026-08-14 19:16:37 +0800

    2026.8.14 19:16

--- venv.bat
+++ venv.bat
@@ -1,2 +1,10 @@
-call venv\Scripts\activate.bat
+@echo off
+if exist "venv\" (
+    call venv\Scripts\activate.bat
+) else (
+    echo 未找到 venv，正在创建虚拟环境...
+    python -m venv venv
+    call venv\Scripts\activate.bat
+)
+
 cmd /k
