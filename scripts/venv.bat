74520544744fb70553cfd252ba6c1a59b1b4285c
tendokei521 <tendokei521@gmail.com>
2026-08-14 19:34:51 +0800

    2026.8.14 19:34

--- scripts/venv.bat
+++ scripts/venv.bat
@@ -1,4 +1,5 @@
 @echo off
+cd /d "%~dp0.."
 if exist "venv\" (
     call venv\Scripts\activate.bat
 ) else (
--- scripts/venv.sh
+++ scripts/venv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd "$(dirname "$0")/.."
 if [ -d "venv" ]; then
     source venv/bin/activate
 else
