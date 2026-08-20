69edec00879c72b9d0a6d27c197a6751bf179088
tendokei521 <tendokei521@gmail.com>
2026-08-21 00:33:13 +0800

    chore(dashboard): rebuild dist

--- dashboard/dist/assets/VSpacer-mch4LP_I.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./VGrid-8if6_hZo.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ dashboard/dist/assets/agentNav-CJ3_OgMX.js
@@ -0,0 +1,1 @@
+import{aw as e}from"./index-SxHazY12.js";const o=e("agentNav",{state:()=>({sections:[]}),actions:{setSections(t){this.sections=t}}});export{o as u};
