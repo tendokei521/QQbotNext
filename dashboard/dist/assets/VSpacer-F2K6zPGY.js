763e1a5c3292d13a819a8fa1d53e985d5f16b5fa
tendokei521 <tendokei521@gmail.com>
2026-08-27 13:58:56 +0800

    chore(dashboard): rebuild static bundle with plugin management UI

--- dashboard/dist/assets/VSpacer-F2K6zPGY.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./VGrid-DAOX2aDs.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ dashboard/dist/assets/VSpacer-FlPkzgiD.js
@@ -0,0 +1,1 @@
+import{r}from"./VGrid-BFRs7CxN.js";const e=r("v-spacer","div","VSpacer");export{e as V};
