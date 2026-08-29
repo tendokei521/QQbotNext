2ef8c3a11852312e0a390b0de12925dc2836f835
tendokei521 <tendokei521@gmail.com>
2026-08-29 18:53:08 +0800

    build(dashboard): rebuild dist for collapsible log export dialog

---
+++ dashboard/dist/assets/VSpacer-Bcyuxfo4.js
@@ -0,0 +1,1 @@
+/* empty css              */import{s as r}from"./VChip--tCAE6PK.js";const p=r("v-spacer","div","VSpacer");export{p as V};
--- dashboard/dist/assets/VSpacer-Cy9i4GD8.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{r}from"./index-BMUICzU4.js";const p=r("v-spacer","div","VSpacer");export{p as V};
