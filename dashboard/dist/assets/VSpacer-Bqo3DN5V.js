5cd4dd1816e3ec21b7028202238d4664683bf806
tendokei521 <tendokei521@gmail.com>
2026-08-29 19:07:57 +0800

    build(dashboard): rebuild dist for current folder and checkbox alignment

---
+++ dashboard/dist/assets/VSpacer-Bqo3DN5V.js
@@ -0,0 +1,1 @@
+/* empty css              */import{s as r}from"./VChip-Bsbr01nJ.js";const p=r("v-spacer","div","VSpacer");export{p as V};
--- dashboard/dist/assets/VSpacer-vLY8WsHb.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{s as r}from"./VChip-DL8jSSUD.js";const p=r("v-spacer","div","VSpacer");export{p as V};
