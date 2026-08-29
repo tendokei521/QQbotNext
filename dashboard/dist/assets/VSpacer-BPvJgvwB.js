38b142f6255b7f988bae2a30aca19310f3b04fc4
tendokei521 <tendokei521@gmail.com>
2026-08-29 18:57:22 +0800

    build(dashboard): rebuild dist for current logs first and expanded

---
+++ dashboard/dist/assets/VSpacer-BPvJgvwB.js
@@ -0,0 +1,1 @@
+/* empty css              */import{s as r}from"./VChip-D-C7xJXH.js";const p=r("v-spacer","div","VSpacer");export{p as V};
--- dashboard/dist/assets/VSpacer-Bcyuxfo4.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{s as r}from"./VChip--tCAE6PK.js";const p=r("v-spacer","div","VSpacer");export{p as V};
