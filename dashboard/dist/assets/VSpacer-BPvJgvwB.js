e1981809dc5cd6cd97a10d11fa52a9b6355fbf8e
tendokei521 <tendokei521@gmail.com>
2026-08-29 19:00:18 +0800

    build(dashboard): rebuild dist for log dialog font/layout tweaks

--- dashboard/dist/assets/VSpacer-BPvJgvwB.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{s as r}from"./VChip-D-C7xJXH.js";const p=r("v-spacer","div","VSpacer");export{p as V};
---
+++ dashboard/dist/assets/VSpacer-vLY8WsHb.js
@@ -0,0 +1,1 @@
+/* empty css              */import{s as r}from"./VChip-DL8jSSUD.js";const p=r("v-spacer","div","VSpacer");export{p as V};
