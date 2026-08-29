02e529a606b3b14a1a5662e956644fbdc8e5d695
tendokei521 <tendokei521@gmail.com>
2026-08-29 19:18:43 +0800

    build(dashboard): rebuild dist for zip field fix and checkbox wrapper

---
+++ dashboard/dist/assets/VSpacer-2TOKHzMW.js
@@ -0,0 +1,1 @@
+/* empty css              */import{s as r}from"./VChip-BM4Q4rmi.js";const p=r("v-spacer","div","VSpacer");export{p as V};
--- dashboard/dist/assets/VSpacer-Bqo3DN5V.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{s as r}from"./VChip-Bsbr01nJ.js";const p=r("v-spacer","div","VSpacer");export{p as V};
