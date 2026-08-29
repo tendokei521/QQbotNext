b408416dd2fd4cb5c810d78dcefdb22e624235e2
tendokei521 <tendokei521@gmail.com>
2026-08-29 18:44:56 +0800

    build(dashboard): rebuild dist for log export dialog

--- dashboard/dist/assets/VSpacer-CpqkMTsB.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{r}from"./index-Dwjbwqe2.js";const p=r("v-spacer","div","VSpacer");export{p as V};
---
+++ dashboard/dist/assets/VSpacer-Cy9i4GD8.js
@@ -0,0 +1,1 @@
+/* empty css              */import{r}from"./index-BMUICzU4.js";const p=r("v-spacer","div","VSpacer");export{p as V};
