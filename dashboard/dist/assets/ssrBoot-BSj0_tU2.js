35dda8a6017a515e76a6dcc819657bc2aa127ff0
tendokei521 <tendokei521@gmail.com>
2026-09-06 21:12:46 +0800

    chore(dashboard): rebuild dist for Tool management page

---
+++ dashboard/dist/assets/ssrBoot-BSj0_tU2.js
@@ -0,0 +1,1 @@
+import{B as t,H as e,i as s,bg as n}from"./index-CKa7a2u7.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
